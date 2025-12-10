aa77f94e1429411bea241c33bc227cb7126bb4a1
Bartilibiaz <bartekjanicki125@gmail.com>
2025-12-10 20:01:13 +0100

    Add files via upload

---
+++ src/main/java/com/bartilibiaz/weaponsplugin/WeaponsPlugin.java
@@ -0,0 +1,50 @@
+package com.bartilibiaz.weaponsplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Bukkit;
+import com.bartilibiaz.weaponsplugin.listeners.WeaponListener;
+import com.bartilibiaz.weaponsplugin.listeners.KnockbackRemovalListener;
+import com.bartilibiaz.weaponsplugin.weapons.WeaponManager;
+import com.bartilibiaz.weaponsplugin.commands.WeaponsCommand;
+
+public class WeaponsPlugin extends JavaPlugin {
+
+    private static WeaponsPlugin instance;
+    private WeaponManager weaponManager;
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(
+            new KnockbackRemovalListener(this),
+            this
+        );
+        getCommand("weaponsZ").setExecutor(new WeaponsCommand(this));
+        instance = this;
+        
+        getLogger().info("====================================");
+        getLogger().info("   WeaponsPlugin v1.0.0 Loading...");
+        getLogger().info("====================================");
+
+        // Initialize weapon manager
+        weaponManager = new WeaponManager(this);
+        
+        // Register listeners
+        Bukkit.getPluginManager().registerEvents(new WeaponListener(this), this);
+
+        getLogger().info("✓ Weapons loaded successfully!");;
+        getLogger().info("====================================");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("WeaponsPlugin disabled!");
+    }
+
+    public static WeaponsPlugin getInstance() {
+        return instance;
+    }
+
+    public WeaponManager getWeaponManager() {
+        return weaponManager;
+    }
+}
